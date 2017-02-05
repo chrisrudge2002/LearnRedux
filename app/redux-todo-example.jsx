c17a660e9d5bcd8ed41ac6ffed38288d619dfd10
Chris Rudge <chrisrudge2002@gmail.com>
2017-02-05 11:29:38 +0000

    More redux learning

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -11,20 +11,57 @@ const reducer = (state = stateDefault, action) => {
 
 	switch (action.type)
 	{
-		case 'CHANGE_SEARCH_TEXT':
-			return {
-				...state,
-				searchText: action.searchText
-			};
-		case 'CHANGE_SHOW_COMPLETED':
-			return {
-				...state,
-				showCompleted: action.showCompleted
-			};
-		default:
-			return state;
+		case 'ADD_TODO': return reducerAddTodo(state, action);
+		case 'CHANGE_SEARCH_TEXT': return reducerChangeSearchText(state, action);
+		case 'CHANGE_SHOW_COMPLETED': return reducerChangeShowCompleted(state, action);
+		case 'TOGGLE_TODO': return reducerToggleTodo(state, action);
+		default: return state;
 	}
 };
+
+function reducerAddTodo(state, action) {
+	return {
+		...state,
+		todos: [
+			...state.todos,
+			{
+				id: action.id,
+				text: action.text,
+				completed: action.completed
+			}
+		]
+	};
+}
+
+function reducerChangeSearchText(state, action) {
+	return {
+		...state,
+		searchText: action.searchText
+	};
+}
+
+function reducerChangeShowCompleted(state, action) {
+	return {
+		...state,
+		showCompleted: action.showCompleted
+	};
+}
+
+function reducerToggleTodo(state, action) {
+	return {
+		...state,
+		todos: state.todos.map((todo) => {
+			if (todo.id === action.id) {
+				return {
+					...todo,
+					completed: !todo.completed
+				};
+			} else {
+				return todo;
+			}
+		})
+	};
+}
 
 let store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -56,19 +93,33 @@ store.dispatch({
 
 // unsubscribe();
 
-// store.dispatch({
-// 	type: 'CHANGE_SHOW_COMPLETED',
-// 	showCompleted: true
-// });
+store.dispatch({
+	type: 'CHANGE_SHOW_COMPLETED',
+	showCompleted: true
+});
 
-// store.dispatch({
-// 	type: 'ADD_TODO',
-// 	todo: {
-// 	}
-// });
+store.dispatch({
+	type: 'ADD_TODO',
+	id: 1,
+	text: 'Test Todo 1',
+	completed: false
+});
 
-// store.dispatch({
-// 	type: 'COMPLETE_TODO',
-// 	todo: {
-// 	}
-// });
+store.dispatch({
+	type: 'ADD_TODO',
+	id: 2,
+	text: 'Test Todo 2',
+	completed: false
+});
+
+store.dispatch({
+	type: 'ADD_TODO',
+	id: 3,
+	text: 'Test Todo 3',
+	completed: false
+});
+
+store.dispatch({
+	type: 'TOGGLE_TODO',
+	id: 2
+});
